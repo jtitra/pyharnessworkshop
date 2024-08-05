6fda78f6b63678eefb93639179b06920ba02888e
Joe Titra <jtitra@harness.io>
2024-08-05 12:26:28 -0400

    Bump version: 0.1.11 → 0.1.12

--- pyharnessworkshop/__init__.py
+++ pyharnessworkshop/__init__.py
@@ -14,7 +14,7 @@
 
 __project__ = "pyharnessworkshop"
 # The version is auto-updated. Please do not edit.
-__version__ = "0.1.11"
+__version__ = "0.1.12"
 
 from . import harness
 from . import keycloak
