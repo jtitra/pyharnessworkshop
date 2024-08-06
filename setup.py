fe3e9f1ca8da137c9c1e5f46bf8cdd7732df8ab6
Joe Titra <jtitra@harness.io>
2024-08-06 15:34:35 -0400

    Bump version: 0.1.14 → 0.1.15

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ Module used by the Harness Instruqt Workshops
 
 import setuptools
 
-PACKAGE_VERSION = "0.1.14"
+PACKAGE_VERSION = "0.1.15"
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
