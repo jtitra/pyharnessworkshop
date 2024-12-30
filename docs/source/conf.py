1f89bef6560788061b682124a776f93717b3544d
Joe Titra <jtitra@harness.io>
2024-12-30 16:00:06 -0500

    Bump version: 0.1.20 → 0.1.21

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'pyharnessworkshop'
 copyright = '2024, Joe Titra'
 author = 'Joe Titra'
-release = '0.1.20'
+release = '0.1.21'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
